ea3cc1ec0f1a75816d142ef8dad626a555be909f
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-06-25 00:08:41 -0300

    feat: :card_file_box: add migrations

---
+++ db/migrations/20250625030317_meal.ts
@@ -0,0 +1,18 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meal', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.text('description').notNullable()
+    table.boolean('is_diet').defaultTo(true).notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    // Creating the foreign key
+    table.uuid('user_id').notNullable()
+    table.foreign('user_id').references('id').inTable('users')
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meal')
+}
